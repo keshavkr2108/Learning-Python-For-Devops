0f0435210f5a268c6e87f5418f69d285feb43d87
keshavkr2108 <keshavraj2110@gmail.com>
2025-02-12 21:49:52 +0530

    Day 1 complete

---
+++ Day_01/ConCat.py
@@ -0,0 +1,7 @@
+str1 = 2
+str2 = 3
+# str3 = str1 + " " + str2
+# print(str1 + " " + str2)
+# print (str3)
+cap = str1 + str2 + 2
+print(cap)
---
+++ Day_01/test.py
@@ -0,0 +1,6 @@
+name = "My name is Keshav Kumar"
+
+upper = name.split(" ")[3]
+
+print (upper.lower()[0])
+print(len(upper))
