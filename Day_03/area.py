40c90ec127dc84e75c9a97c2ccaef24a27201d0e
keshavkr2108 <keshavraj2110@gmail.com>
2025-03-12 08:32:30 +0530

    All update

--- Day_03/area.py
+++ Day_03/area.py
@@ -1,13 +1,13 @@
 def find_area():
     shape = str(input("Enter the shape, You want to find the area of:"))
-    if shape == "rectangle" :
+    if shape.lower() == "rectangle" :
         a,b = map(float, input("Enter the lenght and width:").split(','))
         area = a*b
-        print(f"Area of given Rectangle is:{area}")
-    elif shape == "circle":
+        print(f"Area of given Rectangle is:{round(area,2)}")
+    elif  shape.lower() == "circle":
         rad = float(input("Enter the radius of circle:"))
         area = float(3.14*rad*rad)
-        print(f"Area of given circle is:{area}")
+        print(f"Area of given circle is:{round(area,2)}")
     else:
         print("Invalid Shape")
 find_area()
---
+++ Day_03/even_odd.py
@@ -0,0 +1,5 @@
+num = int(input("Enter the number:"))
+if num % 2 == 1:
+    print(f"{num} is odd")
+else:
+    print(f"{num} is even")
